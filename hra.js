022d61031edcc6bfc139c18456a712bb56beb92c
Daniela Schalldach <sulikova.daniela@gmail.com>
2023-04-16 21:47:42 +0200

    for all buttons

--- hra.js
+++ hra.js
@@ -1,32 +1,13 @@
-let currentPlayer=`circle`;
-const one=document.getElementById(`one`);
-const two=document.querySelector(`#two`);
-const three=document.querySelector(`#three`);
-const four=document.querySelector(`#four`);
-const five=document.querySelector(`#five`);
-const six=document.querySelector(`#six`);
-const seven=document.querySelector(`#seven`);
-const eight=document.querySelector(`#eight`);
-const nine=document.querySelector(`#nine`);
-const ten=document.querySelector(`#ten`);
+
 const playerIcon=document.querySelector(`#hraje-icon`);
 
 let nextIcon = `circle.svg`;
+let currentPlayer=`circle`;
 
 if (currentPlayer===`circle`) {
-    playerIcon.src=nextIcon //přidá ikonu o za Hraje:
+    playerIcon.src=nextIcon 
 }
 
-
-// if (currentPlayer!==`circle`) {
-//     playerIcon.src=`cross.svg`
-// }
-// else {
-//     playerIcon.src=`cross.svg`
-// }
-// if (currentPlayer= ==`cross`) {
-//     playerIcon.src=`cross.svg`
-// }
 
 const crossOrCircle = (event) => {
     const player=event.target.classList
@@ -36,19 +17,11 @@ const crossOrCircle = (event) => {
         currentPlayer=`cross`;
         event.target.disabled=true;
         nextIcon=`cross.svg`;
-        // playerIcon=`cross.svg`;
-        
-
     } else {
-        // currentPlayer!==`circle`;
-        //  player.value=`board__field--cross`;
-        // currentPlayer=`circle`;
         player.src=`circle.svg`;
-        // event.target.disabled=true;
         currentPlayer===`cross`;
-         player.value=player.value +` ` +`board__field--cross`; //vytváří křížky na button
+        player.value=player.value +` ` +`board__field--cross`;
         currentPlayer=`circle`;
-        // player.src=`cross.svg`;
         event.target.disabled=true;
         nextIcon=`circle.svg`;
        
@@ -56,18 +29,14 @@ const crossOrCircle = (event) => {
     playerIcon.src=nextIcon //přidá ikonu o za Hraje:
 
 };
-one.addEventListener(`click`,crossOrCircle);
-two.addEventListener(`click`,crossOrCircle);
-three.addEventListener(`click`,crossOrCircle);
- four.addEventListener(`click`,crossOrCircle);
- five.addEventListener(`click`,crossOrCircle);
-six.addEventListener(`click`,crossOrCircle);
-seven.addEventListener(`click`,crossOrCircle);
-eight.addEventListener(`click`,crossOrCircle);
-nine.addEventListener(`click`,crossOrCircle);
-ten.addEventListener(`click`,crossOrCircle);
+// přidání fce pro všecjna tlačítka .pole
+const playGroundElm=document.querySelectorAll(`.pole`);
+const playArrayElm=Array.from(playGroundElm);
+playArrayElm.forEach ((pole)=> {
+    pole.addEventListener("click", crossOrCircle)
+});
 
-// naprogramování zpětného tlačítka
+// naprogramování tlačítka "Zpět"
 const restart=document.querySelector(`.zpet`)
 
 
